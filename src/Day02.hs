a2a0ed33af2b115c6f20d105ab1f2ad14906fbe5
Florian Hofmann <fho@f12n.de>
2020-12-03 12:40:56 +0100

    beautified

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,32 +1,41 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Day02 where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 import           Data.Attoparsec.Text
-import           Data.Either
 
 import           Control.Applicative
 
 
-input = parseOnly allL <$> T.readFile "inputs/day02.txt"
+
+-- define a simple data type to hold our data per line
+data Line = Line Int Int Char T.Text deriving Show
+
+-- write a quick parser definition to parse a single line
+lineP = Line <$> decimal <* "-" <*> decimal <* " " <*> letter <* ": " <*> takeTill (=='\n')
+
+-- function to read and parse the input file
+parseInput = parseOnly (lineP `sepBy` "\n") <$> T.readFile "inputs/day02.txt"
+
+-- function to validate a line according to part 1
+valid1 (Line mi ma l s) = n >= mi && n <= ma
+  where n = T.length . T.filter (==l) $ s
 
 
-oneL :: Parser (Int,Int,Char,String)
-oneL = (,,,) <$> (read <$> many digit <* "-") <*> (read <$> many digit <* " ") <*> (letter <* ": ") <*> (many letter)
-allL = oneL `sepBy` "\n"
-
-
-valid (mi,ma,l,s) = n >= mi && n <= ma
-  where n = length (filter (==l) s)
-
-part1 = length . filter valid . Data.Either.fromRight undefined  <$> input
-
-valid2 (mi,ma,l,s) = (a == l) `xor` (b == l)
-  where a = s !! (mi - 1)
-        b = s !! (ma - 1)
+-- function to validate a line according to part 2
+valid2 (Line mi ma l s) = (a == l) `xor` (b == l)
+  where a = s `T.index` (mi - 1)
+        b = s `T.index` (ma - 1)
         xor p q = (p || q) && not (p && q)
 
-part2 = length . filter valid2 . Data.Either.fromRight undefined  <$> input
+-- run the whole thing
+main = do
+
+  (Right input) <- parseInput
+
+  let answer1 = length . filter valid1 $ input
+  putStrLn $ "answer 1: " ++ show answer1
+
+  let answer2 = length . filter valid2 $ input
+  putStrLn $ "answer 2: " ++ show answer2
